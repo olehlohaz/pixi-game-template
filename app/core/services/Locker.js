1b776c9c894485f318d07fe5d5b80eff9c464e13
Sharbel <sharbelfs@gmail.com>
2016-11-10 01:06:14 -0200

    locker updated

--- app/core/services/Locker.js
+++ app/core/services/Locker.js
@@ -6,22 +6,16 @@ class Locker {
 
 
     this.listSites = [
-      'aHR0cDovL3NoYXJiZWwuaW5mbw==', // http://sharbel.info
-      // 'aHR0cDovL2xvY2FsaG9zdA==', // http://localhost
+      'http://localhost'
     ]
 
-    // console.log( window[atob('bG9jYXRpb24=')]['aHJlZg=='] )
-
-    this.current = window[atob('bG9jYXRpb24=')][atob('aHJlZg==')]
+    this.current = window.location.href
 
     this.isValid = this.verify()
 
     if(!CONFIG.TRAVAR) {
       this.isValid = true      
     }
-    // console.log(CONFIG.TRAVAR)
-    // console.log( btoa('http://sharbel.info'), btoa('http://localhost') )
-    
   }
 
   verify() {
